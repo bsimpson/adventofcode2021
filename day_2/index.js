0f61a59a5f9be9a31600cd42ebc99975ca83de81
Ben Simpson <thehoagie@gmail.com>
2021-12-02 08:11:43 -0500

    day two part two

--- day_2/index.js
+++ day_2/index.js
@@ -40,4 +40,48 @@ function partOne() {
   });
 }
 
-partOne();
+// partOne();
+
+// part two
+function partTwo() {
+  const FILENAME = "./partOneInput";
+
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+
+    // e.g. forward 5
+    data.split("\n").forEach((movement) => {
+      // position = forward
+      // amount = 5
+      let [position, amount] = movement.split(" ");
+      amount = parseInt(amount);
+
+      switch(position) {
+        case "forward":
+          horizontal += amount;
+          depth += (aim * amount);
+          break;
+        case "up":
+          aim -= amount;
+          break;
+        case "down":
+          aim += amount;
+          break;
+        default:
+          throw `Unknown position: ${position}`;
+      }
+    });
+
+    console.log({ horizontal, depth, aim });
+    console.log(horizontal * depth);
+  });
+}
+
+partTwo();
