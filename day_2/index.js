e2054f4fde4cdcee1b70218ae9c89896fe8c42f5
Ben Simpson <thehoagie@gmail.com>
2021-12-02 08:05:16 -0500

    day two part one

---
+++ day_2/index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+// part one
+function partOne() {
+  const FILENAME = "./partOneInput";
+
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+
+    let horizontal = 0;
+    let depth = 0;
+
+    // e.g. forward 5
+    data.split("\n").forEach((movement) => {
+      // position = forward
+      // amount = 5
+      let [position, amount] = movement.split(" ");
+      amount = parseInt(amount);
+
+      switch(position) {
+        case "forward":
+          horizontal += amount;
+          break;
+        case "up":
+          depth -= amount;
+          break;
+        case "down":
+          depth += amount;
+          break;
+        default:
+          throw `Unknown position: ${position}`;
+      }
+    });
+
+    console.log({ horizontal, depth });
+    console.log(horizontal * depth);
+  });
+}
+
+partOne();
