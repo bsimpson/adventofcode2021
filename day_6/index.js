9ef9be58d3ee0930593b971e4db5f650d4bff05a
Ben Simpson <thehoagie@gmail.com>
2021-12-06 13:18:15 -0500

    day six part two

--- day_6/index.js
+++ day_6/index.js
@@ -38,4 +38,61 @@ function partOne() {
   });
 }
 
-partOne();
+// partOne();
+
+// part two
+function partTwo() {
+  const FILENAME = "./partOneInput";
+
+  fs.readFile(FILENAME, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+
+    let fishArray = data.split(",").map(i => parseInt(i));
+    let fishObject = fishArray.reduce((agg, fish) => {
+      // e.g. fish = 4
+      agg[fish]++;
+
+      return agg;
+    }, { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 });
+
+    console.log({ fishObject });
+
+    const DAYS = 256;
+
+    for(let i = 1; i <= DAYS; i++) {
+      console.log(i);
+      console.log({ fishObject });
+      debugger;
+
+      // new fish come in at 8
+      // record the old 8 to move to 7
+      const temp8 = 0 + fishObject[8];
+      const temp0 = 0 + fishObject[0];
+
+      // and set new fish on 8
+      fishObject[8] = fishObject[0];
+
+      // step everything down
+      fishObject[0] = fishObject[1];
+      fishObject[1] = fishObject[2];
+      fishObject[2] = fishObject[3];
+      fishObject[3] = fishObject[4];
+      fishObject[4] = fishObject[5];
+      fishObject[5] = fishObject[6];
+      fishObject[6] = fishObject[7] + temp0; // resetting fish come in at 0
+      fishObject[7] = temp8;
+    }
+
+    const numberOfFish = Object.keys(fishObject).reduce((sum, key) => {
+      sum = sum + fishObject[key];
+      return sum;
+    }, 0);
+
+    console.log({ numberOfFish });
+  });
+}
+
+partTwo();
